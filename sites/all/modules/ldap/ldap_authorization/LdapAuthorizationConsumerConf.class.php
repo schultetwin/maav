31c993ce26390a3e8fd95a8fdc5daaa1447c9bad
Mark Schulte <schultetwin@gmail.com>
2012-04-04 17:27:16 -0400

    Adding ccl and ldap

--- sites/all/modules/ldap/ldap_authorization/LdapAuthorizationConsumerConf.class.php
+++ sites/all/modules/ldap/ldap_authorization/LdapAuthorizationConsumerConf.class.php
@@ -32,11 +32,18 @@ class LdapAuthorizationConsumerConf {
   public $deriveFromAttr = FALSE;
   public $deriveFromAttrAttr = NULL;
   public $deriveFromAttrUseFirstAttr = FALSE;
+  public $deriveFromAttrNested = FALSE;
 
   public $deriveFromEntry = FALSE;
   public $deriveFromEntryEntries = NULL;
-  public $deriveFromEntryAttr = NULL;
+  public $deriveFromEntryEntriesAttr = NULL;
+
+  public $deriveFromEntryMembershipAttr = NULL;
   public $deriveFromEntrySearchAll = FALSE;
+  public $deriveFromEntryAttrMatchingUserAttr = FALSE; // can be removed in 2.0 branch
+  public $deriveFromEntryAttrMatchingUserAttrUndefined = TRUE;
+  public $deriveFromEntryUseFirstAttr = FALSE;
+  public $deriveFromEntryNested = FALSE;
 
 
   public $mappings = array();
@@ -75,6 +82,15 @@ class LdapAuthorizationConsumerConf {
       $this->inDatabase = TRUE;
       $this->loadFromDb();
     }
+    // default value for deriveFromEntryAttrMatchingUserAttr set up this way for backward compatibility in 1.0 branch,
+    // make deriveFromEntryAttrMatchingUserAttr default to dn in 2.0 branch.
+    if ($this->deriveFromEntryAttrMatchingUserAttr) {
+      $this->deriveFromEntryAttrMatchingUserAttrUndefined = FALSE;
+    }
+    else {
+      $this->deriveFromEntryAttrMatchingUserAttr = 'dn';
+      $this->deriveFromEntryAttrMatchingUserAttrUndefined = TRUE;
+    }
     $this->normalizedMappings = $consumer->normalizeMappings($this->mappings);
   }
 
@@ -103,7 +119,6 @@ class LdapAuthorizationConsumerConf {
       if ($this->consumerType) {
         $select->condition('ldap_authorization.consumer_type',  $this->consumerType);
       }
-
       $consumer_conf = $select->execute()->fetchObject();
     }
 
@@ -114,7 +129,6 @@ class LdapAuthorizationConsumerConf {
 
     $this->sid = $consumer_conf->sid;
     $this->consumerType = $consumer_conf->consumer_type;
-    $this->description = $consumer_conf->description;
     $this->status = (bool)$consumer_conf->status;
     $this->onlyApplyToLdapAuthenticated  = (bool)(@$consumer_conf->only_ldap_authenticated);
 
@@ -124,12 +138,18 @@ class LdapAuthorizationConsumerConf {
     $this->deriveFromAttr  = (bool)($consumer_conf->derive_from_attr);
     $this->deriveFromAttrAttr =  $this->linesToArray($consumer_conf->derive_from_attr_attr);
     $this->deriveFromAttrUseFirstAttr  = (bool)($consumer_conf->derive_from_attr_use_first_attr);
-    $this->deriveFromEntrySearchAll = (bool)($consumer_conf->derive_from_entry_search_all);
-
+    $this->deriveFromAttrNested  = (bool)($consumer_conf->derive_from_attr_nested);
 
     $this->deriveFromEntry  = (bool)(@$consumer_conf->derive_from_entry);
     $this->deriveFromEntryEntries = $this->linesToArray($consumer_conf->derive_from_entry_entries);
-    $this->deriveFromEntryAttr = $consumer_conf->derive_from_entry_attr;
+    $this->deriveFromEntryEntriesAttr = $consumer_conf->derive_from_entry_entries_attr;
+
+
+    $this->deriveFromEntryMembershipAttr = $consumer_conf->derive_from_entry_attr;
+    $this->deriveFromEntryAttrMatchingUserAttr = $consumer_conf->derive_from_entry_user_ldap_attr;
+    $this->deriveFromEntrySearchAll = (bool)($consumer_conf->derive_from_entry_search_all);
+    $this->deriveFromEntryUseFirstAttr  = (bool)($consumer_conf->derive_from_entry_use_first_attr);
+    $this->deriveFromEntryNested = $consumer_conf->derive_from_entry_nested;
 
     $this->mappings = $this->pipeListToArray($consumer_conf->mappings);
     $this->useMappingsAsFilter = (bool)(@$consumer_conf->use_filter);
@@ -155,18 +175,26 @@ class LdapAuthorizationConsumerConf {
   protected $saveable = array(
     'sid',
     'consumerType',
-    'description',
     'status',
     'onlyApplyToLdapAuthenticated',
+
     'deriveFromDn',
     'deriveFromDnAttr',
+
     'deriveFromAttr',
     'deriveFromAttrAttr',
     'deriveFromAttrUseFirstAttr',
+    'deriveFromAttrNested',
+
     'deriveFromEntry',
     'deriveFromEntryEntries',
-    'deriveFromEntryAttr',
+    'deriveFromEntryEntriesAttr',
+    'deriveFromEntryMembershipAttr',
     'deriveFromEntrySearchAll',
+    'deriveFromEntryAttrMatchingUserAttr',
+    'deriveFromEntryUseFirstAttr',
+    'deriveFromEntryNested',
+
     'mappings',
     'useMappingsAsFilter',
     'synchToLdap',
